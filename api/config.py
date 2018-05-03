70360a6c2b718d226bf3d2fca6775e55a4bcf300
sasiliyu <sasil.adetunji@gmail.com>
2018-05-03 15:51:12 +0100

    add .env

--- api/config.py
+++ api/config.py
@@ -2,8 +2,10 @@ import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-postgres_local_base = 'postgresql://postgres:AjibolA2016?@localhost/'
-database_name = 'feedand_api'
+postgres_local_base = os.environ.get('SQLALCHEMY_DATABASE_URI')
+postgres_local_base_test = os.environ.get('SQLALCHEMY_DATABASE_URI_TEST')
+
+# database_name = 'feedand_api'
 
 class BaseConfig:
     """Base configuration"""
@@ -14,7 +16,7 @@ class DevelopmentConfig(BaseConfig):
     """Development configuration"""
     BCRYPT_LOG_ROUNDS = 4
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = postgres_local_base + database_name
+    SQLALCHEMY_DATABASE_URI = postgres_local_base
 
 
 class TestingConfig(BaseConfig):
@@ -22,7 +24,7 @@ class TestingConfig(BaseConfig):
     DEBUG = True
     TESTING = True
     BCRYPT_LOG_ROUNDS = 4
-    SQLALCHEMY_DATABASE_URI = postgres_local_base + database_name + '_test'
+    SQLALCHEMY_DATABASE_URI = postgres_local_base_test
 
 class ProductionConfig(BaseConfig):
     """Production configuration"""
