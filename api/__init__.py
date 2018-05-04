b9046a65ca1ba7f4892cbbe854b6b195c41f1574
sasiliyu <sasil.adetunji@gmail.com>
2018-05-04 10:54:57 +0100

    edit the deploy config

--- api/__init__.py
+++ api/__init__.py
@@ -17,12 +17,20 @@ app.config.from_object(app_settings)
 bcrypt = Bcrypt(app)
 db = SQLAlchemy(app)
 
+<<<<<<< HEAD
 from api.blueprint.ex import dummy_blueprint, admin_reg_blueprint, admin_login_blueprint, menu_blueprint, menu_item_blueprint, feedback_blueprint, user_book_meal_blueprint
+=======
+from api.blueprint.ex import dummy_blueprint, admin_reg_blueprint, admin_login_blueprint
+>>>>>>> edit the deploy config
 
 app.register_blueprint(dummy_blueprint)
 app.register_blueprint(admin_reg_blueprint)
 app.register_blueprint(admin_login_blueprint)
+<<<<<<< HEAD
 app.register_blueprint(menu_blueprint)
 app.register_blueprint(menu_item_blueprint)
 app.register_blueprint(feedback_blueprint)
 app.register_blueprint(user_book_meal_blueprint)
+=======
+
+>>>>>>> edit the deploy config
